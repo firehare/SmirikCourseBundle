e64e3bad90f6977582919a63b17fff6c43aa5a1f
Evgeny Smirnov <smirik@gmail.com>
2012-02-15 01:14:54 +0400

    Add list of courses into menu dropdown

--- EventListener/ConfigureMenuListener.php
+++ EventListener/ConfigureMenuListener.php
@@ -12,6 +12,13 @@ class ConfigureMenuListener
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
         $menu = $event->getMenu();
-        $menu->addChild('admin.course.menu', array('route' => 'admin_courses'));
+        $menu->addChild('admin.course.menu.main', array('route' => 'admin_courses'));
+        $menu->addChild('admin.course.menu.lessons', array('route' => 'admin_courses'));
+        $cm = $event->container->get('course.manager');
+        $courses = $cm->getCourses();
+        foreach ($courses as $course)
+        {
+          $menu['admin.course.menu.lessons']->addChild($course->getTitle(), array('route' => 'admin_lessons', 'routeParameters' => array('course_id' => $course->getId())));
+        }
     }
 }
